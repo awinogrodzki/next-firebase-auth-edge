4a5c96f5aba30dfd0f43dc38645c2f7772cba8ef
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2022-11-30 15:53:03 +0100

    Update middleware.ts

--- examples/next13-typescript-simple/middleware.ts
+++ examples/next13-typescript-simple/middleware.ts
@@ -6,21 +6,21 @@ import { serverConfig } from './app/server-config';
 const LOGIN_PATH = '/api/login';
 const LOGOUT_PATH = '/api/logout';
 
-export async function middleware(request: NextRequest) {
-  const commonOptions = {
-    apiKey: serverConfig.firebaseApiKey,
-    cookieName: 'AuthToken',
-    cookieSignatureKeys: ['secret1', 'secret2'],
-    cookieSerializeOptions: {
-      path: '/',
-      httpOnly: true,
-      secure: false, // Set this to true on HTTPS environments
-      sameSite: 'strict' as const,
-      maxAge:  12 * 60 * 60 * 24 * 1000, // twelve days
-    },
-    serviceAccount: serverConfig.serviceAccount,
-  };
+const commonOptions = {
+  apiKey: serverConfig.firebaseApiKey,
+  cookieName: 'AuthToken',
+  cookieSignatureKeys: ['secret1', 'secret2'],
+  cookieSerializeOptions: {
+    path: '/',
+    httpOnly: true,
+    secure: false, // Set this to true on HTTPS environments
+    sameSite: 'strict' as const,
+    maxAge:  12 * 60 * 60 * 24 * 1000, // twelve days
+  },
+  serviceAccount: serverConfig.serviceAccount,
+};
 
+export async function middleware(request: NextRequest) {
   if ([LOGIN_PATH, LOGOUT_PATH].includes(request.nextUrl.pathname)) {
     return createAuthMiddlewareResponse(request, {
       loginPath: LOGIN_PATH,
