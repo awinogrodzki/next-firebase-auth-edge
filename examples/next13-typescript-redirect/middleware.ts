d02acfb2a8b06d19b135cc1a802707a468b9032e
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2023-05-28 11:34:05 +0200

    chore: remove console.log

--- examples/next13-typescript-redirect/middleware.ts
+++ examples/next13-typescript-redirect/middleware.ts
@@ -2,11 +2,6 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  console.log("REQUEST URL", request.nextUrl.href, {
-    typeofCaches: typeof caches,
-    globalTypeofCaches: typeof global.caches,
-    globalThisTypeofCaches: typeof (globalThis as any)?.caches,
-  });
   return NextResponse.next();
 }
 
