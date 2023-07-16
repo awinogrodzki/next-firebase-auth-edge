b99c880bdeb51f0e598ca00364b7d6d3294eed97
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2023-07-16 22:22:32 +0200

    chore(rework-examples): fix form submit on provider login

--- examples/next13-typescript-starter/ui/PasswordForm/PasswordForm.tsx
+++ examples/next13-typescript-starter/ui/PasswordForm/PasswordForm.tsx
@@ -6,8 +6,6 @@ import { IconButton } from "../IconButton";
 import { VisibleIcon } from "../icons/VisibleIcon";
 import { HiddenIcon } from "../icons/HiddenIcon";
 import { Button } from "../Button";
-import { useSearchParams } from "next/navigation";
-import Link from "next/link";
 import { FirebaseError } from "@firebase/util";
 import { FormError } from "../FormError";
 
@@ -32,8 +30,6 @@ export function PasswordForm({
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [isHidden, setIsHidden] = React.useState(true);
-  const params = useSearchParams();
-  const redirect = params?.get("redirect");
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
@@ -46,55 +42,53 @@ export function PasswordForm({
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      {...props}
-      className={cx(styles.form, props.className)}
-    >
-      <Input
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        name="email"
-        type="email"
-        placeholder="Email address"
-      />
-      <div className={styles.input}>
+    <div className={cx(styles.container, props.className)}>
+      <form onSubmit={handleSubmit} {...props} className={styles.form}>
         <Input
           required
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type={isHidden ? "password" : "text"}
-          placeholder="Password"
-          minLength={8}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          type="email"
+          placeholder="Email address"
         />
-        {(isHidden && (
-          <IconButton
-            onClick={() => setIsHidden(false)}
-            className={styles.adornment}
-          >
-            <VisibleIcon />
-          </IconButton>
-        )) || (
-          <IconButton
-            onClick={() => setIsHidden(true)}
-            className={styles.adornment}
-          >
-            <HiddenIcon />
-          </IconButton>
-        )}
-      </div>
-      {error && <FormError>{error.message}</FormError>}
-      <Button
-        loading={loading}
-        disabled={loading}
-        variant="contained"
-        type="submit"
-      >
-        Submit
-      </Button>
+        <div className={styles.input}>
+          <Input
+            required
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type={isHidden ? "password" : "text"}
+            placeholder="Password"
+            minLength={8}
+          />
+          {(isHidden && (
+            <IconButton
+              onClick={() => setIsHidden(false)}
+              className={styles.adornment}
+            >
+              <VisibleIcon />
+            </IconButton>
+          )) || (
+            <IconButton
+              onClick={() => setIsHidden(true)}
+              className={styles.adornment}
+            >
+              <HiddenIcon />
+            </IconButton>
+          )}
+        </div>
+        {error && <FormError>{error.message}</FormError>}
+        <Button
+          loading={loading}
+          disabled={loading}
+          variant="contained"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
       {children}
-    </form>
+    </div>
   );
 }
