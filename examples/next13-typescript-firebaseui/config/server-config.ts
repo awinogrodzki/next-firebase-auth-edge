802045df24d7a054a36eaf58bfd6ce013755152a
Alec Kriebel <me@aleckriebel.com>
2023-08-05 10:35:44 -0700

    fix: Correct maxAge examples from milliseconds to seconds

--- examples/next13-typescript-firebaseui/config/server-config.ts
+++ examples/next13-typescript-firebaseui/config/server-config.ts
@@ -17,7 +17,7 @@ export const authConfig = {
     httpOnly: true,
     secure: serverConfig.useSecureCookies, // Set this to true on HTTPS environments
     sameSite: "lax" as const,
-    maxAge: 12 * 60 * 60 * 24 * 1000, // twelve days
+    maxAge: 12 * 60 * 60 * 24, // twelve days
   },
   serviceAccount: serverConfig.serviceAccount,
 };
