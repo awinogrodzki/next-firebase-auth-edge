693c993788c978268361c72e0b3c19f15ac6a752
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2023-08-06 15:40:51 +0200

    fix: update middleware.ts config not to process file requests

--- examples/next13-typescript-starter/middleware.ts
+++ examples/next13-typescript-starter/middleware.ts
@@ -51,5 +51,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/((?!_next|favicon.ico|api).*)", "/api/login", "/api/logout"],
+  matcher: ["/", "/((?!_next|favicon.ico|api|.*\\.).*)", "/api/login", "/api/logout"],
 };
