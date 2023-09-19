52e233f784bacfab904117a0e3185693dd07e67b
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2023-09-19 17:34:38 +0200

    chore(example): add pages api route tokens handler

---
+++ examples/next13-typescript-starter/pages/api/tokens.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getTokensFromObject } from 'next-firebase-auth-edge/lib/next/tokens';
+import { authConfig } from '../../config/server-config';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const tokens = await getTokensFromObject(req.cookies, {
+    apiKey: authConfig.apiKey,
+    cookieName: authConfig.cookieName,
+    cookieSignatureKeys: authConfig.cookieSignatureKeys,
+    serviceAccount: authConfig.serviceAccount,
+  });
+
+  return res.status(200).json({ tokens });
+}
