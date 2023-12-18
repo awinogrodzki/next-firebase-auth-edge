cd24a9200de29e1b7f6c116f74930f50a2cf1862
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2023-12-18 22:51:22 +0100

    test(app-check): pass app check token in integration test

--- src/auth/test/set-custom-user-claims.integration.test.ts
+++ src/auth/test/set-custom-user-claims.integration.test.ts
@@ -1,23 +1,33 @@
 import { customTokenToIdAndRefreshTokens, getFirebaseAuth } from "../index";
 import { v4 } from "uuid";
+import { AppCheckToken } from "../../app-check/types";
+import { getAppCheck } from "../../app-check";
 
 const {
   FIREBASE_API_KEY,
   FIREBASE_PROJECT_ID,
   FIREBASE_ADMIN_CLIENT_EMAIL,
   FIREBASE_ADMIN_PRIVATE_KEY,
+  FIREBASE_APP_ID,
 } = process.env;
 
+const TEST_SERVICE_ACCOUNT = {
+  clientEmail: FIREBASE_ADMIN_CLIENT_EMAIL!,
+  privateKey: FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+  projectId: FIREBASE_PROJECT_ID!,
+};
+
 describe("set custom user claims integration test", () => {
+  let appCheckToken: AppCheckToken = { token: "", ttlMillis: 0 };
+
+  beforeAll(async () => {
+    const { createToken } = getAppCheck(TEST_SERVICE_ACCOUNT);
+
+    appCheckToken = await createToken(FIREBASE_APP_ID!);
+  });
+
   const { createCustomToken, getUser, setCustomUserClaims, verifyIdToken } =
-    getFirebaseAuth(
-      {
-        clientEmail: FIREBASE_ADMIN_CLIENT_EMAIL!,
-        privateKey: FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-        projectId: FIREBASE_PROJECT_ID!,
-      },
-      FIREBASE_API_KEY!
-    );
+    getFirebaseAuth(TEST_SERVICE_ACCOUNT, FIREBASE_API_KEY!);
 
   it("should create custom user claims", async () => {
     const userId = v4();
@@ -28,7 +38,8 @@ describe("set custom user claims integration test", () => {
 
     const { idToken } = await customTokenToIdAndRefreshTokens(
       customToken,
-      FIREBASE_API_KEY!
+      FIREBASE_API_KEY!,
+      { appCheckToken: appCheckToken.token }
     );
 
     await verifyIdToken(idToken);
