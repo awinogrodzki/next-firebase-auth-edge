140ba4ace7181ab6ea363a56021cec30fe649f81
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2023-12-19 01:43:40 +0100

    chore(example): enable suggestion from https://github.com/awinogrodzki/next-firebase-auth-edge/issues/127\#issuecomment-1861879630

--- examples/next13-typescript-starter/app-check/index.ts
+++ examples/next13-typescript-starter/app-check/index.ts
@@ -13,6 +13,14 @@ export function getOrInitializeAppCheck(app: FirebaseApp): AppCheck {
     return appCheck;
   }
 
+  // Firebase uses a global variable to check if app check is enabled in a dev environment
+  if (process.env.NODE_ENV !== "production") {
+    Object.assign(window, {
+      FIREBASE_APPCHECK_DEBUG_TOKEN:
+        process.env.NEXT_PUBLIC_APP_CHECK_DEBUG_TOKEN,
+    });
+  }
+
   return (appCheck = initializeAppCheck(app, {
     provider: new ReCaptchaEnterpriseProvider(
       process.env.NEXT_PUBLIC_FIREBASE_APP_CHECK_KEY!
