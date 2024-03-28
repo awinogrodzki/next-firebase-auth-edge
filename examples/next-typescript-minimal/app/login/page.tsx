b9c5b9fd429a47c454223bc88507cfd6aa1f2d13
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2024-03-28 15:06:48 +0100

    chore(examples): fix build

--- examples/next-typescript-minimal/app/login/page.tsx
+++ examples/next-typescript-minimal/app/login/page.tsx
@@ -99,7 +99,7 @@ export default function Login() {
               Enter
             </button>
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              Don't have an account?{" "}
+              Don&apos;t have an account?{" "}
               <Link
                 href="/register"
                 className="font-medium text-primary-600 hover:underline dark:text-primary-500"
