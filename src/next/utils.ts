2f75386de3d91aea42345771c006221eff819104
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2024-05-31 16:56:30 +0200

    fix: referer is now based on caller host

--- src/next/utils.ts
+++ src/next/utils.ts
@@ -1,9 +1,8 @@
 export function getReferer(headers: Headers) {
-  const referer = headers.get('Referer');
   const host =
     headers.get('X-Forwarded-Host') ?? headers.get('Host') ?? undefined;
   const protocol = headers.get('X-Forwarded-Proto');
   const fallback = protocol && host ? `${protocol}://${host}/` : undefined;
 
-  return referer ?? fallback ?? host;
+  return fallback ?? host;
 }
