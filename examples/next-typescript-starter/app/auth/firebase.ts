8482ddbb9a0f969d67e5168cf95ef19aa68333be
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2025-04-12 11:48:16 +0200

    chore(examples): fix app check in starter example

--- examples/next-typescript-starter/app/auth/firebase.ts
+++ examples/next-typescript-starter/app/auth/firebase.ts
@@ -1,25 +1,18 @@
-import {getApp, getApps, initializeApp} from 'firebase/app';
+import { getApp, getApps, initializeApp } from 'firebase/app';
 import {
   connectAuthEmulator,
   getAuth,
   inMemoryPersistence,
   setPersistence
 } from 'firebase/auth';
-import {clientConfig} from '../../config/client-config';
-import {getOrInitializeAppCheck} from '../../app-check';
+import { clientConfig } from '../../config/client-config';
 
 export const getFirebaseApp = () => {
   if (getApps().length) {
     return getApp();
   }
 
-  const app = initializeApp(clientConfig);
-
-  if (process.env.NEXT_PUBLIC_FIREBASE_APP_CHECK_KEY) {
-    getOrInitializeAppCheck(app);
-  }
-
-  return app;
+  return initializeApp(clientConfig);
 };
 
 export function getFirebaseAuth() {
